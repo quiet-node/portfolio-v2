6fbc44cae8db2ba0452dfc509c16e84f9c49d22c
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-12 18:48:27 -0400

    Inited portfolio v2

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import Aos from 'aos';
+import './App.css';
+import Intro from './components/intro/Intro';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
