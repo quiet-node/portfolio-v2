33715843cdcb916429c7b30c6d644880b6c58894
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-13 14:20:29 -0400

    Added framerVariants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
+/**
+ * @notice constants for social media links
+ */
 export const QUIET_NODE_GITHUB_LINK = 'https://github.com/quiet-node';
 export const QUIET_NODE_LINKEDIN_LINK = 'https://linkedin.com/in/logann131';
 export const QUIET_NODE_TWITTER_LINK = 'https://twitter.com/quiet_node';
