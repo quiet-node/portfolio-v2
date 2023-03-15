a12bc72c4b3bb4a8a1f07a0fd5b33e730908d57d
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-15 19:17:09 -0400

    Added shouldShake attribute to technology objects

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,31 +11,37 @@ export const MAIN_LEFT_SKILLS_LISTS = [
     techology: 'Golang',
     value: 85,
     techLink: 'https://go.dev/',
+    shouldShake: false,
   },
   {
     techology: 'TypeScript',
     value: 92,
     techLink: 'https://typescriptlang.org/',
+    shouldShake: false,
   },
   {
     techology: 'Solidity',
     value: 75,
     techLink: 'https://soliditylang.org/',
+    shouldShake: false,
   },
   {
     techology: 'Java',
     value: 88,
     techLink: 'https://java.com/',
+    shouldShake: false,
   },
   {
     techology: 'MongoDB',
     value: 82,
     techLink: 'https://mongodb.com/',
+    shouldShake: false,
   },
   {
     techology: 'TailwindCSS',
     value: 87,
     techLink: 'https://tailwindcss.com/',
+    shouldShake: false,
   },
 ];
 
@@ -44,31 +50,37 @@ export const MAIN_RIGHT_SKILLS_LISTS = [
     techology: 'ReactJS',
     value: 90,
     techLink: 'https://reactjs.org/',
+    shouldShake: false,
   },
   {
     techology: 'NextJS',
     value: 88,
     techLink: 'https://nextjs.org/',
+    shouldShake: false,
   },
   {
     techology: 'SpringBoot',
     value: 72,
     techLink: 'https://spring.io/',
+    shouldShake: false,
   },
   {
     techology: 'Hardhat',
     value: 72,
     techLink: 'https://hardhat.org/',
+    shouldShake: false,
   },
   {
     techology: 'NodeJS',
     value: 87,
     techLink: 'https://nodejs.org/',
+    shouldShake: false,
   },
   {
     techology: 'Docker',
     value: 75,
     techLink: 'https://docker.com/',
+    shouldShake: false,
   },
 ];
 
@@ -76,49 +88,61 @@ export const MORE_SKILLS_LISTS = [
   {
     techology: 'HTML5',
     techLink: 'https://www.w3schools.com/html/',
+    shouldShake: false,
   },
   {
     techology: 'CSS3',
     techLink: 'https://www.w3schools.com/Css/',
+    shouldShake: false,
   },
   {
     techology: 'Javascript',
     techLink: 'https://javascript.com/',
+    shouldShake: false,
   },
   {
     techology: 'Ethereum',
     techLink: 'https://ethereum.org/en/',
+    shouldShake: false,
   },
   {
     techology: 'OpenZeppelin',
     techLink: 'https://www.openzeppelin.com/',
+    shouldShake: false,
   },
   {
     techology: 'MochaJS',
     techLink: 'https://mochajs.org/',
+    shouldShake: false,
   },
   {
     techology: 'MySQL',
     techLink: 'https://www.mysql.com/',
+    shouldShake: false,
   },
   {
     techology: 'PostgresQL',
     techLink: 'https://www.postgresql.org/',
+    shouldShake: false,
   },
   {
     techology: 'aws',
     techLink: 'https://aws.amazon.com/',
+    shouldShake: false,
   },
   {
     techology: 'vercel',
     techLink: 'https://vercel.com/',
+    shouldShake: false,
   },
   {
     techology: 'Netlify',
     techLink: 'https://www.netlify.com/',
+    shouldShake: false,
   },
   {
     techology: 'Heroku',
     techLink: 'https://www.heroku.com/',
+    shouldShake: false,
   },
 ];
