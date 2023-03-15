e8eb59bc327a96c5c297cb08a0ced37814674da3
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-15 14:27:32 -0400

    Added new SKILLS array information to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,120 @@ export const QUIET_NODE_GITHUB_LINK = 'https://github.com/quiet-node';
 export const QUIET_NODE_EMAIL_LINK = 'mailto:lg.131.ng@gmail.com';
 export const QUIET_NODE_LINKEDIN_LINK = 'https://linkedin.com/in/logann131';
 export const QUIET_NODE_TWITTER_LINK = 'https://twitter.com/quiet_node';
+
+export const MAIN_LEFT_SKILLS_LISTS = [
+  {
+    techology: 'Golang',
+    value: 85,
+    techLinks: 'https://go.dev/',
+  },
+  {
+    techology: 'TypeScript',
+    value: 92,
+    techLinks: 'https://typescriptlang.org/',
+  },
+  {
+    techology: 'Solidity',
+    value: 75,
+    techLinks: 'https://soliditylang.org/',
+  },
+  {
+    techology: 'Java',
+    value: 88,
+    techLinks: 'https://java.com/',
+  },
+  {
+    techology: 'MongoDB',
+    value: 82,
+    techLinks: 'https://mongodb.com/',
+  },
+  {
+    techology: 'TailwindCSS',
+    value: 87,
+    techLinks: 'https://tailwindcss.com/',
+  },
+];
+
+export const MAIN_RIGHT_SKILLS_LISTS = [
+  {
+    techology: 'ReactJS',
+    value: 90,
+    techLinks: 'https://reactjs.org/',
+  },
+  {
+    techology: 'NextJS',
+    value: 88,
+    techLinks: 'https://nextjs.org/',
+  },
+  {
+    techology: 'SpringBoot',
+    value: 72,
+    techLinks: 'https://spring.io/',
+  },
+  {
+    techology: 'Hardhat',
+    value: 72,
+    techLinks: 'https://hardhat.org/',
+  },
+  {
+    techology: 'NodeJS',
+    value: 87,
+    techLinks: 'https://nodejs.org/',
+  },
+  {
+    techology: 'Docker',
+    value: 75,
+    techLinks: 'https://docker.com/',
+  },
+];
+
+export const MORE_SKILLS_LISTS = [
+  {
+    techology: 'HTML5',
+    techLinks: 'https://www.w3schools.com/html/',
+  },
+  {
+    techology: 'CSS3',
+    techLinks: 'https://www.w3schools.com/Css/',
+  },
+  {
+    techology: 'Javascript',
+    techLinks: 'https://javascript.com/',
+  },
+  {
+    techology: 'Ethereum',
+    techLinks: 'https://ethereum.org/en/',
+  },
+  {
+    techology: 'OpenZeppelin',
+    techLinks: 'https://www.openzeppelin.com/',
+  },
+  {
+    techology: 'MochaJS',
+    techLinks: 'https://mochajs.org/',
+  },
+  {
+    techology: 'MySQL',
+    techLinks: 'https://www.mysql.com/',
+  },
+  {
+    techology: 'PostgresQL',
+    techLinks: 'https://www.postgresql.org/',
+  },
+  {
+    techology: 'aws',
+    techLinks: 'https://aws.amazon.com/',
+  },
+  {
+    techology: 'vercel',
+    techLinks: 'https://vercel.com/',
+  },
+  {
+    techology: 'Netlify',
+    techLinks: 'https://www.netlify.com/',
+  },
+  {
+    techology: 'Heroku',
+    techLinks: 'https://www.heroku.com/',
+  },
+];
