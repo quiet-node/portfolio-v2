22547ce555695ce25133ae6be0f74dc831902e3d
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-17 18:24:20 -0400

    Update Projects.tsx

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,18 +1,18 @@
 import { motion } from 'framer-motion';
+import { ProjectTile } from '../utils';
+import { PROJECTS_LISTS } from '../../utils/constants';
 
 const Projects = () => {
   return (
     <section
-      className='relative z-10 overflow-hidden scrollbar-hidden bg-primary h-screen
+      className='relative z-10 overflow-hidden scrollbar-hidden bg-[#EFD8C9]
                 -top-52'
     >
       {/* container */}
       <div
-        className='flex flex-col items-center text-white
-                /mt-6
-                /sm:mt-9
-                md:mt-32
-                2xl:max-w-[100rem] 2xl:mx-auto'
+        className='flex flex-col items-center
+                gap-9
+                md:mt-32'
       >
         {/* header */}
         <div>
@@ -21,7 +21,7 @@ const Projects = () => {
             whileInView={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.3 }}
             viewport={{ amount: 'some', margin: '100% 0% -8% 0%' }}
-            className='text-3xl sm:text-4xl font-extrabold'
+            className='text-3xl sm:text-4xl font-black text-[#A87163]'
           >
             Projects.
           </motion.h1>
@@ -32,11 +32,14 @@ const Projects = () => {
             viewport={{ amount: 'some', margin: '100% 0% -9% 0%' }}
             className='flex justify-center -mt-0.5'
           >
-            <hr className='bg-white h-[0.2rem] w-20 sm:w-20 rounded-xl' />
+            <hr className='bg-[#A87163] h-[0.2rem] w-20 sm:w-20 rounded-xl' />
           </motion.div>
         </div>
 
         {/* body - projects */}
+        <div className='flex gap-3'>
+          <ProjectTile project={PROJECTS_LISTS[0]} flipped={false} />
+        </div>
       </div>
     </section>
   );
