e5a654a45cfc9a6ae0dafafc28f736611980cdc4
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-17 16:49:59 -0400

    Create interfaces.d.ts

---
+++ src/utils/interfaces.d.ts
@@ -0,0 +1,21 @@
+interface Project {
+  name: string;
+  stacks: ProjectStack;
+  description: string;
+  repoLink: string;
+  liveLink: string;
+  thumbnail: string;
+}
+
+interface ProjectStack {
+  FE: ProjectTechs[];
+  BC: ProjectTechs[];
+  BE: ProjectTechs[];
+}
+
+interface Technology {
+  technology: string;
+  value?: number;
+  techLink: string;
+  shouldShake: boolean;
+}
