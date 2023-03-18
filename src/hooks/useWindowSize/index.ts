1d4c4f340c90dec967d9164a094d9734da9f56f8
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-18 18:23:31 -0400

    Added IsMdMedium screen size to useScreenSize hook

--- src/hooks/useWindowSize/index.ts
+++ src/hooks/useWindowSize/index.ts
@@ -5,13 +5,14 @@ const useScreenSizes = () => {
   const windowSize = useWindowSize();
   const [isSmall, setIsSmall] = useState(false);
   const [isMedium, setIsMedium] = useState(false);
+  const [IsMdMedium, setIsMdMedium] = useState(false);
 
   useEffect(() => {
     setIsSmall((windowSize.width as number) >= 640);
-
     setIsMedium((windowSize.width as number) >= 768);
+    setIsMdMedium((windowSize.width as number) >= 976);
   }, [windowSize]);
-  return [isSmall, isMedium];
+  return [isSmall, isMedium, IsMdMedium];
 };
 
 export default useScreenSizes;
