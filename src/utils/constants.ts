fc9e2f6dc6c6daf54285090ad0a4ea49877a20a0
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-19 14:44:24 -0400

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-/**
- * @notice constants for social media links
- */
+export const QUIET_NODE_PORTFOLIO_V2_GITHUB_LINK =
+  'https://github.com/quiet-node/portfolio-v2';
+
 export const QUIET_NODE_GITHUB_LINK = 'https://github.com/quiet-node';
 export const QUIET_NODE_REDDIT_LINK =
   'https://www.reddit.com/user/Quiet-Computer-3495';
@@ -10,39 +10,40 @@ export const QUIET_NODE_TWITTER_LINK = 'https://twitter.com/quiet_node';
 export const QUIET_NODE_DISCORD_LINK =
   'https://discordapp.com/users/401650245202739201';
 
-const GO_LINK = 'https://go.dev/';
-const TS_LINK = 'https://typescriptlang.org/';
-const SOL_LINK = 'https://soliditylang.org/';
-const JAVA_LINK = 'https://java.com/';
-const MONGO_LINK = 'https://mongodb.com/';
-const TAILWIND_LINK = 'https://tailwindcss.com/';
-const REACT_LINK = 'https://reactjs.org/';
-const NEXT_LINK = 'https://nextjs.org/';
-const SPRING_LINK = 'https://spring.io/';
-const HARDHAT_LINK = 'https://hardhat.org/';
-const NODE_LINK = 'https://nodejs.org/';
-const DOCKER_LINK = 'https://docker.com/';
-const JS_LINK = 'https://javascript.com/';
-const ETHEREUM_LINK = 'https://ethereum.org/en/';
-const OZ_LINK = 'https://www.openzeppelin.com/';
-const MOCHA_LINK = 'https://mochajs.org/';
-const POSTGRES_LINK = 'https://www.postgresql.org/';
-const AWS_LINK = 'https://aws.amazon.com/';
-const VERCEL_LINK = 'https://vercel.com/';
-const NETLIFY_LINK = 'https://www.netlify.com/';
-const DYNAMO_LINK = 'https://aws.amazon.com/dynamodb/';
-const VITE_LINK = 'https://vitejs.dev/';
-const POLYGON_LINK = 'https://polygon.technology/';
-const OL_LINK = 'https://openliberty.io/';
-const REDUX_LINK = 'https://redux.js.org/';
-const KOT_LINK = 'https://kotlinlang.org/';
-const XML_LINK =
+export const GO_LINK = 'https://go.dev/';
+export const TS_LINK = 'https://typescriptlang.org/';
+export const SOL_LINK = 'https://soliditylang.org/';
+export const JAVA_LINK = 'https://java.com/';
+export const MONGO_LINK = 'https://mongodb.com/';
+export const TAILWIND_LINK = 'https://tailwindcss.com/';
+export const FRAMER_LINK = 'https://www.framer.com/motion/';
+export const REACT_LINK = 'https://reactjs.org/';
+export const NEXT_LINK = 'https://nextjs.org/';
+export const SPRING_LINK = 'https://spring.io/';
+export const HARDHAT_LINK = 'https://hardhat.org/';
+export const NODE_LINK = 'https://nodejs.org/';
+export const DOCKER_LINK = 'https://docker.com/';
+export const JS_LINK = 'https://javascript.com/';
+export const ETHEREUM_LINK = 'https://ethereum.org/en/';
+export const OZ_LINK = 'https://www.openzeppelin.com/';
+export const MOCHA_LINK = 'https://mochajs.org/';
+export const POSTGRES_LINK = 'https://www.postgresql.org/';
+export const AWS_LINK = 'https://aws.amazon.com/';
+export const VERCEL_LINK = 'https://vercel.com/';
+export const NETLIFY_LINK = 'https://www.netlify.com/';
+export const DYNAMO_LINK = 'https://aws.amazon.com/dynamodb/';
+export const VITE_LINK = 'https://vitejs.dev/';
+export const POLYGON_LINK = 'https://polygon.technology/';
+export const OL_LINK = 'https://openliberty.io/';
+export const REDUX_LINK = 'https://redux.js.org/';
+export const KOT_LINK = 'https://kotlinlang.org/';
+export const XML_LINK =
   'https://developer.mozilla.org/en-US/docs/Web/XML/XML_introduction';
-const ANDROID_LINK = 'https://developer.android.com/';
-const FIRE_LINK = 'https://firebase.google.com/';
-const SWIFT_LINK = 'https://developer.apple.com/xcode/swiftui/';
-const MAC_LINK = 'https://support.apple.com/macos';
-const XCODE_LINK = 'https://developer.apple.com/xcode/';
+export const ANDROID_LINK = 'https://developer.android.com/';
+export const FIRE_LINK = 'https://firebase.google.com/';
+export const SWIFT_LINK = 'https://developer.apple.com/xcode/swiftui/';
+export const MAC_LINK = 'https://support.apple.com/macos';
+export const XCODE_LINK = 'https://developer.apple.com/xcode/';
 
 export const MAIN_LEFT_SKILLS_LISTS: Technology[] = [
   {
