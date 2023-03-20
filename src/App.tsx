ed3ad6f9e17ae660bd02a533647ec6722dd8ad52
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-19 23:07:43 -0400

    Added react-ga for page view analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ReactGA from 'react-ga';
 import './App.css';
 import {
   About,
@@ -7,8 +8,14 @@ import {
   Projects,
   SocialMedia,
 } from './components';
+import { useEffect } from 'react';
 
 function App() {
+  // for page view analytics
+  const googleMeasurementId = import.meta.env.VITE_GOOGLE_MEASUREMENT_ID;
+  ReactGA.initialize(googleMeasurementId);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <>
       <Intro />
