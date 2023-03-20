8e93a4db1a0597b06e4436265151983514de0ebb
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-20 00:11:25 -0400

    removed GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import ReactGA from 'react-ga';
 import './App.css';
 import {
   About,
@@ -8,23 +7,8 @@ import {
   Projects,
   SocialMedia,
 } from './components';
-import { useEffect } from 'react';
-
-const googleMeasurementId = import.meta.env.VITE_GOOGLE_MEASUREMENT_ID;
-// Initialize Google Analytics
-ReactGA.initialize(googleMeasurementId);
-
-// Function to track pageviews
-const pageview = (pathname: string) => {
-  ReactGA.set({ page: pathname });
-  ReactGA.pageview(pathname);
-};
 
 function App() {
-  useEffect(() => {
-    pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <>
       <Intro />
