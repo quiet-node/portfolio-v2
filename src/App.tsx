c9c0beeb583abadb59584e9ae3b78333e88e0ff4
Logan Nguyen <lg.131.dev@gmail.com>
2023-03-19 23:49:00 -0400

    tested GA v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import ReactGA from 'react-ga';
 import './App.css';
 import {
   About,
@@ -8,17 +7,9 @@ import {
   Projects,
   SocialMedia,
 } from './components';
-import { useEffect } from 'react';
 
 function App() {
   // for page view analytics
-  const googleMeasurementId = import.meta.env.VITE_GOOGLE_MEASUREMENT_ID;
-
-  useEffect(() => {
-    ReactGA.initialize(googleMeasurementId);
-    ReactGA.pageview(window.location.pathname);
-    console.log(window.location.pathname);
-  }, []);
 
   return (
     <>
