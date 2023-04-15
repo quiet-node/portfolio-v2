82c5c1e9a0403b7c2d104cc37db686c35f22f96f
Logan Nguyen <lg.131.dev@gmail.com>
2023-04-15 00:52:37 -0400

    feat: add new email

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export const QUIET_NODE_PORTFOLIO_V2_GITHUB_LINK =
 export const QUIET_NODE_GITHUB_LINK = 'https://github.com/quiet-node';
 export const QUIET_NODE_REDDIT_LINK =
   'https://www.reddit.com/user/Quiet-Computer-3495';
-export const QUIET_NODE_EMAIL_LINK = 'mailto:lg.131.ng@gmail.com';
+export const QUIET_NODE_EMAIL_LINK = 'mailto:logan@quiet-node.dev';
 export const QUIET_NODE_LINKEDIN_LINK = 'https://linkedin.com/in/logann131';
 export const QUIET_NODE_TWITTER_LINK = 'https://twitter.com/quiet_node';
 export const QUIET_NODE_DISCORD_LINK =
